ea50f5cb1a5c41ff291fc444e6777554c9c9f169
Minkaspr <106852390+Minkaspr@users.noreply.github.com>
2023-05-31 19:52:45 -0500

    Se añadió los métodos restantes

--- app/src/main/java/com/example/sem09_ejerc1/MainActivity.java
+++ app/src/main/java/com/example/sem09_ejerc1/MainActivity.java
@@ -63,6 +63,10 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Método para buscar un producto
+     */
+
     public void Buscar(View v){
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"productos",null,1);
 
@@ -77,11 +81,14 @@ public class MainActivity extends AppCompatActivity {
                 et_nombre.setText(fila.getString(0));
                 et_precio.setText(fila.getString(1));
                 et_codigo.setText(codigo);
-                bd.close();
             } else {
                 Toast.makeText(this, "No existe el producto", Toast.LENGTH_SHORT).show();
-                bd.close();
             }
+
+            // Liberamos recursos
+            fila.close();
+            // Cerramos conexion con base de datos
+            bd.close();
 
         } else {
             Toast.makeText(this, "Ingrese el codigo", Toast.LENGTH_SHORT).show();
@@ -90,8 +97,69 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * Método para actualizar un producto
+     */
+
+    public void Modificar(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"productos",null,1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+        String nombre = et_nombre.getText().toString();
+        String precio = et_precio.getText().toString();
+
+        if (!codigo.isEmpty() && !nombre.isEmpty() && !precio.isEmpty()) {
+
+            ContentValues query = new ContentValues();
+
+            query.put("codigo",codigo);
+            query.put("nombre",nombre);
+            query.put("precio",precio);
+
+            int cantidad = bd.update("producto",query,"codigo="+codigo,null);
+            bd.close();
+            
+            if (cantidad == 1) {
+                Toast.makeText(this, "Producto actualizado con exito", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "No existe el producto", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Debes llenar los campos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Metodo para eliminar un producto
+     */
+
+    public void Eliminar(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "productos", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+
+        String codigo = et_codigo.getText().toString();
+
+        if (!codigo.isEmpty()) {
+            int cantidad = bd.delete("producto", "codigo="+codigo, null);
+            bd.close();
+            
+            et_codigo.setText("");
+            et_nombre.setText("");
+            et_precio.setText("");
+            
+            if (cantidad == 1) {
+                Toast.makeText(this, "Producto eliminado con exito", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "No existe el producto", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, "Debes introducir el codigo del producto", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /**
      * Metodos para validar
-     *
      * regex -> Expresión Regular
      */
     public boolean validarCodigo(String codigo) {
